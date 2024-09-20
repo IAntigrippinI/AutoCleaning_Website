7eee755d4284d8203d6790190e9995c4281b5ff0
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-09-21 00:21:52 +1000

    add changing to mobile

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -9,26 +9,47 @@ import SalesPage from './pages/desktop/SalesPage/SalesPage'
 import RegistrationPage from './pages/desktop/RegistrationPage/RegistrationPage'
 import ProfilePage from './pages/desktop/ProfilePage/ProfilePage'
 import LoginPage from './pages/desktop/LoginPage/LoginPage'
+import useDeviceDetect from './components/useDeviceDetect'
+import LoginPageMobile from './pages/mobile/LoginPageMobile'
+import HomePageMobile from './pages/mobile/HomePageMobile/HomePageMobile'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  const { isMobile } = useDeviceDetect();
+  const { isTablet } = useDeviceDetect();
+
   return (
     <>
-      <div className="App">
+      {isMobile ? <div className="mobile">
         <Router>
           <div className="container">
-            <NavBar />
+
           </div>
           <Routes>
-            <Route path='/' element={<div className='container'><HomePage /></div>} />
+            <Route path='/' element={<div className='container'><HomePageMobile /></div>} />
             <Route path='/sales' element={<SalesPage />} />
             <Route path='/registration' element={<div className='container'><RegistrationPage /></div>} />
             <Route path='/profile' element={<div className='container'><ProfilePage /></div>} />
-            <Route path='/login' element={<div className='container'><LoginPage /></div>} />
+            <Route path='/login' element={<div className='container'><LoginPageMobile /></div>} />
           </Routes>
         </Router>
       </div>
+
+        : <div className="App">
+          <Router>
+            <div className="container">
+              <NavBar />
+            </div>
+            <Routes>
+              <Route path='/' element={<div className='container'><HomePage /></div>} />
+              <Route path='/sales' element={<SalesPage />} />
+              <Route path='/registration' element={<div className='container'><RegistrationPage /></div>} />
+              <Route path='/profile' element={<div className='container'><ProfilePage /></div>} />
+              <Route path='/login' element={<div className='container'><LoginPage /></div>} />
+            </Routes>
+          </Router>
+        </div>}
     </>
   )
 }
