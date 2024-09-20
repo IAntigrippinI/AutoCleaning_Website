299555d91c5e98859b9f0c6badd6f8d6249d7ffa
IAntigrippinI <kovalskii.bi@dvfu.ru>
2024-09-21 00:20:29 +1000

    add navbar for mobile

---
+++ frontend/src/components/mobile/NavBarMobile/NavBarMobile.jsx
@@ -0,0 +1,15 @@
+const NavBarMobile = () => {
+    return (
+
+        <div className="nav-mobile">
+            <div className="logo">
+
+            </div>
+            <div className="burger">
+
+            </div>
+        </div>
+    );
+}
+
+export default NavBarMobile;
